dc30993dc9601814347955e5bfffad7c5937f517
孟恒 <mengheng@fshows.com>
2019-09-06 12:15:24 +0800

    demo

--- YuJiaTruckManager/AppDelegate.swift
+++ YuJiaTruckManager/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     /// 百度地图
     var mapManager: BMKMapManager?
+    
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
