cd210e807acd819a79bb4bd877aef4dfd13af752
孟恒 <1215933389@qq.com>
2019-09-06 12:22:21 +0800

    demo

--- YuJiaTruckManager/AppDelegate.swift
+++ YuJiaTruckManager/AppDelegate.swift
@@ -20,6 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     
+    
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
