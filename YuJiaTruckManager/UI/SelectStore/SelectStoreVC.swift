96cad5c0982c0e89ef6ed44f0d41af5557c340d9
menghengmen <1215933389@qq.com>
2020-01-15 21:10:56 +0800

    添加挑货模块

---
+++ YuJiaTruckManager/UI/SelectStore/SelectStoreVC.swift
@@ -0,0 +1,24 @@
+//
+//  SelectStoreVC.swift
+//  YuJiaTruckManager
+//
+//  Created by mh on 2020/1/15.
+//  Copyright © 2020 Jiangsu Yu Dao Data Technology. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import YuDaoComponents
+
+class SelectStoreVC: BaseTableVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ YuJiaTruckManager/UI/SelectStore/SelectStoreVM.swift
@@ -0,0 +1,16 @@
+//
+//  SelectStoreVM.swift
+//  YuJiaTruckManager
+//
+//  Created by mh on 2020/1/15.
+//  Copyright © 2020 Jiangsu Yu Dao Data Technology. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import YuDaoComponents
+
+class SelectStoreVM: BaseTableVM {
+
+}
